1656878d91d53841d151b21b23fa8877b5d56d0e
dallinmoak <dallinmoak@gmail.com>
2018-03-28 14:07:53 -0600

    new thingything

--- freelance_camp_documents/db/seeds.rb
+++ freelance_camp_documents/db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ 10.times do |d|
+  FreelanceDocument.create!(
+    title: "Document #{d}",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rhoncus tortor sed ligula hendrerit, non tincidunt turpis consectetur. Cras suscipit nibh neque, vel iaculis mauris auctor vel. Pellentesque pharetra sem sed enim semper semper. Suspendisse potenti. In nibh lectus, hendrerit ac nisi eget, elementum lacinia augue. Pellentesque non sapien sed sem tempus pretium. Morbi auctor eros in tortor egestas, quis scelerisque turpis accumsan. Suspendisse maximus aliquam condimentum. Aenean nisl lectus, venenatis quis elit rhoncus, lacinia ornare justo. Duis consectetur dictum sapien. Nulla facilisi. Vestibulum molestie, elit vitae fringilla ultrices, nibh sapien condimentum nisl, vel aliquet odio erat et quam. Pellentesque a faucibus enim. Nunc maximus vitae enim nec consequat. Morbi aliquet libero ut mauris cursus, feugiat volutpat velit volutpat.",
+    file_url: "https://drive.google.com/file/d/0B5_fywRVZd2iWWFScnN6Vl9lT2M/view?usp=sharing",
+    image_url: "https://i.imgur.com/ymFrpxC.jpg"
+  )  
+end
