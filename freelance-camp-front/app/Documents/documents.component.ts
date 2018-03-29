bc2a95e39482344a4d4ce4b8186baa5c6b766c2e
dallinmoak <dallinmoak@gmail.com>
2018-03-29 14:46:02 -0600

    hook up the rails app!

--- freelance-camp-front/app/Documents/documents.component.ts
+++ freelance-camp-front/app/Documents/documents.component.ts
@@ -1,35 +1,34 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Document} from './document';
+import {DocumentService} from './document.service';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   moduleId: module.id,
   selector: 'documents',
   templateUrl: 'documents.component.html',
-  styleUrls: ["documents.component.css"]
+  styleUrls: ["documents.component.css"],
+  providers: [DocumentService]
 })
-export class DocumentsComponent{
+export class DocumentsComponent implements OnInit{
   pageTitle: string = "Document Dashboard";
-  documents: Document[] = [
-    {
-      title: 'First Document Title',
-      description: 'this describes the title.',
-      file_url: 'http://google.com',
-      updated_at: '11/11/16',
-      image_url: 'https://i.imgur.com/ymFrpxC.jpg'
-    },
-    {
-      title: 'second Document Title',
-      description: 'this describes the title.',
-      file_url: 'http://google.com',
-      updated_at: '11/11/16',
-      image_url: 'https://i.imgur.com/ymFrpxC.jpg'
-    },
-    {
-      title: 'third Document Title',
-      description: 'this describes the title.',
-      file_url: 'http://google.com',
-      updated_at: '11/11/16',
-      image_url: 'https://i.imgur.com/ymFrpxC.jpg'
-    }
-  ]
+  documents: Document[];
+  errorMessage: string;
+  mode = "Observable";
+
+  constructor(
+    private documentService: DocumentService
+  ){}
+
+  ngOnInit(){
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getDocuments());
+  }
+
+  getDocuments(){
+    this.documentService.getDocuments().subscribe(
+      documents => this.documents = documents,
+      error => this.errorMessage = <any>error
+    );
+  }
 }
