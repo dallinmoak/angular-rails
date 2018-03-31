bef4870f953c8b1cc9399fc5e376146a108d18f1
dallinmoak <dallinmoak@gmail.com>
2018-03-30 18:59:44 -0600

    wired up the proposal api to angular

--- freelance-camp-front/app/proposal/proposal-list.component.js
+++ freelance-camp-front/app/proposal/proposal-list.component.js
@@ -12,9 +12,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var proposal_service_1 = require("./proposal.service");
 var Rx_1 = require("rxjs/Rx");
+var router_1 = require("@angular/router");
 var ProposalListComponent = /** @class */ (function () {
-    function ProposalListComponent(proposalService) {
+    function ProposalListComponent(proposalService, router) {
         this.proposalService = proposalService;
+        this.router = router;
         this.mode = "Observable";
     }
     ProposalListComponent.prototype.ngOnInit = function () {
@@ -26,6 +28,10 @@ var ProposalListComponent = /** @class */ (function () {
         var _this = this;
         this.proposalService.getProposals().subscribe(function (proposals) { return _this.proposals = proposals; }, function (error) { return _this.errorMessage = error; });
     };
+    ProposalListComponent.prototype.goToShow = function (proposal) {
+        var link = ['/proposal', proposal.id];
+        this.router.navigate(link);
+    };
     ProposalListComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
@@ -34,7 +40,8 @@ var ProposalListComponent = /** @class */ (function () {
             styleUrls: ['proposal-list.component.css'],
             providers: [proposal_service_1.ProposalService]
         }),
-        __metadata("design:paramtypes", [proposal_service_1.ProposalService])
+        __metadata("design:paramtypes", [proposal_service_1.ProposalService,
+            router_1.Router])
     ], ProposalListComponent);
     return ProposalListComponent;
 }());
